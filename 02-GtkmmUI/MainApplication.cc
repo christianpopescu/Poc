3fbfc19fc6a4f5ef7237c1bb40bf97cc70b0e30c
Christian Popescu <christian.popescu@outlook.com>
2019-12-12 00:10:18 +0100

    [02-GtkmmUI] fixed but to refactor

--- 02-GtkmmUI/MainApplication.cc
+++ 02-GtkmmUI/MainApplication.cc
@@ -2,10 +2,11 @@
 #include "MainWindow.h"
 
 MainApplication::MainApplication(int argc, char** argv, std::string message){
-  //app = Gtk::Application::create(argc, argv, message);
+  app = Gtk::Application::create(argc, argv, message);
 }
 
 int MainApplication::Run(){
-  return 0;
-  // return app->run(mainWindow);
+  MainWindow mainWindow;
+
+  return app->run(mainWindow);
 }
--- 02-GtkmmUI/UIMain.cc
+++ 02-GtkmmUI/UIMain.cc
@@ -4,10 +4,15 @@
 
 int main (int argc, char *argv[])
 {
-   Glib::RefPtr<Gtk::Application>  app = Gtk::Application::create(argc, argv, "org.gtkmm.example");
+  /*   Glib::RefPtr<Gtk::Application>  app = Gtk::Application::create(argc, argv, "org.gtkmm.example");
 
   MainWindow mainWindow;
 
   //Shows the window and returns when it is closed.
   return app->run(mainWindow);
+  */
+  MainApplication mainApplication(argc, argv, "org.gtkmm.example");
+  mainApplication.Run();
+  return 0;
+
 }
