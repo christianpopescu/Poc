3ac14f7fb578e99a885f06767c6ce6a2fd1cc425
Christian Popescu <christian.popescu@outlook.com>
2022-01-22 16:38:11 +0100

    Add poc WinForm c++

---
+++ 06-WinFormCpp/PocWinFormCpp/PocWinFormCpp/PocWinFormCppApp.cpp
@@ -0,0 +1,13 @@
+#include "MainForm.h"
+//#include <Windows.h>
+
+
+using namespace System;
+using namespace System::Windows::Forms;
+
+[STAThread]//leave this as is
+void main(array<String^>^ args) {
+    Application::EnableVisualStyles();
+    Application::SetCompatibleTextRenderingDefault(false);
+    Application::Run(gcnew PocWinFormCpp::MainForm);
+};
