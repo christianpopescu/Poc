afbecf36769c4e0720486f8340e301b2ed648c35
Christian Popescu <christian.popescu@outlook.com>
2022-01-31 21:30:15 +0100

    Poc c++/cli wpf code only. Canvas + TextBox

--- 07-PocWpfCpp/PocWpfCpp/MainWindow.cpp
+++ 07-PocWpfCpp/PocWpfCpp/MainWindow.cpp
@@ -4,15 +4,34 @@ void MainWindow::OnInitialized(EventArgs^ e)
 {
     Window::OnInitialized(e);   // Important to call
     this->Title = "Code Only Window";
-    this->Background = System::Windows::Media::Brushes::White;
+    this->Background = System::Windows::Media::Brushes::Gray;
     Button^ closeButton = gcnew Button();
     closeButton->Content = "Close";
     closeButton->Background = System::Windows::Media::Brushes::Blue;
-    this->Content = closeButton;
+   // this->Content = closeButton;
+    closeButton->Width = 100;
+    closeButton->Height = 50;
+
 //    closeButton->Click += delegate{
 //      this.Close();
-closeButton->Click += gcnew System::Windows::RoutedEventHandler(this, &MainWindow::CloseWindow);
+    closeButton->Click += gcnew System::Windows::RoutedEventHandler(this, &MainWindow::CloseWindow);
+    TextBox^ tb = gcnew TextBox();
+    tb->Text = "this is a text box";
+    tb->Visibility = System::Windows::Visibility::Visible;
+    tb->Width = 300;
+    tb-> Height = 50;
+    tb->Background = System::Windows::Media::Brushes::Blue;
+    tb->HorizontalAlignment = System::Windows::HorizontalAlignment::Center;
+  //  this->Content = tb;
+    Canvas^ canvas = gcnew Canvas();
+ //   canvas->Height = 300;
+ //   canvas->Width = 500;
+    canvas->Background = System::Windows::Media::Brushes::LightGray;
 
+    canvas->Children->Add(closeButton);
+    canvas->Children->Add(tb);
+    canvas->SetLeft(tb, 400);
+    this->Content = canvas;
 }
 
 void MainWindow::CloseWindow(System::Object^ sender, System::Windows::RoutedEventArgs^ e)
