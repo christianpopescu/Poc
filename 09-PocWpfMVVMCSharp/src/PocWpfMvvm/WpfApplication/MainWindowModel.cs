c337c432fa8a973963af0119ef15f1a8e24d58cf
Christian Popescu <christian.popescu@outlook.com>
2022-12-19 23:24:48 +0100

    Add view model

---
+++ 09-PocWpfMVVMCSharp/src/PocWpfMvvm/WpfApplication/MainWindowModel.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApplication
+{
+    internal class MainWindowModel : INotifyPropertyChanged
+    {
+        private string _text;
+        private string _textView;
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        public string Text
+        {
+            get { return _text; }
+            set { _text = value; OnPropertyChanged(); }
+        }
+
+        public string TextView
+        {
+            get { return _textView; }
+            set { _textView = value; OnPropertyChanged(); }
+        }
+
+        // Create the OnPropertyChanged method to raise the event
+        // The calling member's name will be used as the parameter.
+        protected void OnPropertyChanged([CallerMemberName] string name = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+    }
+}
