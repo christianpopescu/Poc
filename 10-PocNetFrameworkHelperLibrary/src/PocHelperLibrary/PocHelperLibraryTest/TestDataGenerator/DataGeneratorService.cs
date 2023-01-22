70018f65b9da86e5913379326992097b36906a52
Christian Popescu <christian.popescu@outlook.com>
2023-01-22 11:27:07 +0100

    New test cases + test data generator

---
+++ 10-PocNetFrameworkHelperLibrary/src/PocHelperLibrary/PocHelperLibraryTest/TestDataGenerator/DataGeneratorService.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PocHelperLibraryTest.TestDataGenerator
+{
+    internal class DataGeneratorService
+    {
+        public IDictionary<string, string> GetPlacehoderDictionary() 
+        { 
+            var dictionary = new Dictionary<string, string>();
+
+            dictionary["abc"] = "123";
+            dictionary["bcd"] = "345";
+
+            return dictionary;
+        }
+    }
+}
