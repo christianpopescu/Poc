a50b90f55460e542ca7a8a9df982b8817b8ed4f7
Christian Popescu <christian.popescu@outlook.com>
2023-02-10 21:45:35 +0100

    Add Poc on Hybrid Console Windows (Win form) application

---
+++ 12-PocCSharpConsoleWinForm/src/ConsoleWinForm/ConsoleWinForm/Program.cs
@@ -0,0 +1,13 @@
+﻿using System.Windows.Forms;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Display the number of command line arguments.
+        Console.WriteLine("The console has been started");
+        MessageBox.Show("WinForm in Console Application");
+        Console.WriteLine("Press any key to exit!");
+        Console.ReadKey();
+    }
+}
