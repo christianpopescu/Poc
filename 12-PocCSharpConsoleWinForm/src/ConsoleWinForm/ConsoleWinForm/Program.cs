ef51c3c5e45dd401f6779b31bb9257a04d424b44
Christian Popescu <christian.popescu@outlook.com>
2023-02-11 20:40:43 +0100

    Poc console win form -> Open Dialog added

--- 12-PocCSharpConsoleWinForm/src/ConsoleWinForm/ConsoleWinForm/Program.cs
+++ 12-PocCSharpConsoleWinForm/src/ConsoleWinForm/ConsoleWinForm/Program.cs
@@ -2,11 +2,30 @@
 
 class Program
 {
+    [STAThread]
     static void Main(string[] args)
     {
         // Display the number of command line arguments.
         Console.WriteLine("The console has been started");
-        MessageBox.Show("WinForm in Console Application");
+        var fileContent = string.Empty;
+        var filePath = string.Empty;
+        using (OpenFileDialog openFileDialog = new OpenFileDialog())
+        {
+            openFileDialog.InitialDirectory = "e:\\";
+            openFileDialog.Filter = "txt files (*.pdf)|*.pdf|All files (*.*)|*.*";
+            openFileDialog.FilterIndex = 2;
+            openFileDialog.RestoreDirectory = true;
+
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                //Get the path of specified file
+                filePath = openFileDialog.FileName;
+
+
+
+            }
+        }
+        MessageBox.Show("WinForm in Console Application + selected file : " + filePath);
         Console.WriteLine("Press any key to exit!");
         Console.ReadKey();
     }
