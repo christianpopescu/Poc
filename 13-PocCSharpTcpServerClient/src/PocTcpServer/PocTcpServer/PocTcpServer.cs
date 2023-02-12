988d9f78cee9578efa449761f42d21eab4f3379e
Christian Popescu <christian.popescu@outlook.com>
2023-02-12 23:26:09 +0100

    Add project server tcp net core

---
+++ 13-PocCSharpTcpServerClient/src/PocTcpServer/PocTcpServer/PocTcpServer.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace PocTcpServer
+{
+    internal class PocTcpServer
+    {
+    }
+}
