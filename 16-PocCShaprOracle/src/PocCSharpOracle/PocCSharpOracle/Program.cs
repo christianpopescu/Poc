a70c3fc7da19623ddff05a25e227e0399831570f
Christian Popescu <christian.popescu@outlook.com>
2023-03-16 23:51:50 +0100

    Add PocOracle for C#

---
+++ 16-PocCShaprOracle/src/PocCSharpOracle/PocCSharpOracle/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace PocCSharpOracle
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
