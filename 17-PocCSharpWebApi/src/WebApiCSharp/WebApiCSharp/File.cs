dcfbe5dd6be953d50061fd4876b51190337805a9
Christian Popescu <christian.popescu@outlook.com>
2023-04-05 23:42:17 +0200

    Add Poc on WebApi

---
+++ 17-PocCSharpWebApi/src/WebApiCSharp/WebApiCSharp/File.cs
@@ -0,0 +1,8 @@
+﻿namespace WebApiCSharp
+{
+    public class File
+    {
+        public string Name { get; set; }
+        public string Path { get; set; }
+    }
+}
---
+++ 17-PocCSharpWebApi/src/WebApiCSharp/WebApiCSharp/Program.cs
@@ -0,0 +1,55 @@
+namespace WebApiCSharp
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+            builder.Services.AddAuthorization();
+
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+
+            app.UseAuthorization();
+
+            var summaries = new[]
+            {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+            app.MapGet("/weatherforecast", (HttpContext httpContext) =>
+            {
+                var forecast = Enumerable.Range(1, 5).Select(index =>
+                    new WeatherForecast
+                    {
+                        Date = DateTime.Now.AddDays(index),
+                        TemperatureC = Random.Shared.Next(-20, 55),
+                        Summary = summaries[Random.Shared.Next(summaries.Length)]
+                    })
+                    .ToArray();
+                return forecast;
+            });
+
+            var filesCollection = Directory.GetFiles(@"D:\Temp");
+
+            app.MapGet("/files", (HttpContext httpContext) =>
+            {
+
+
+
+                var files = filesCollection.Select(f =>
+                    new File
+                    {
+                        Name = Path.GetFileName(f),
+                       Path = Path.GetFullPath(f)
+                    }).ToArray();
+                return files;
+            });
+            app.Run();
+        }
+    }
+}
