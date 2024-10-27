1eef56ce8e5ea43a69f4270e4768d62edfa80522
Christian Popescu <christian.popescu@outlook.com>
2024-10-27 23:22:02 +0100

    poc-25 First running Swing application

--- 25-PocJavaSwing/src/PocSwingApp/src/main/java/fr/vadc/App.java
+++ 25-PocJavaSwing/src/PocSwingApp/src/main/java/fr/vadc/App.java
@@ -1,10 +1,30 @@
 package fr.vadc;
 
+import java.awt.*;
+import javax.swing.*;
+
 /**
  * Hello world!
  */
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
-    }
+         EventQueue.invokeLater(() ->
+		{
+		var frame = new SimpleFrame();
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setVisible(true);
+		});
+	 }
+}
+
+
+class SimpleFrame extends JFrame
+{
+private static final int DEFAULT_WIDTH = 300;
+private static final int DEFAULT_HEIGHT = 200;
+
+public SimpleFrame()
+{
+setSize(DEFAULT_WIDTH, DEFAULT_HEIGHT);
+}
 }
