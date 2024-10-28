9c70d40e9be0ac74e42eca018684276e416762cf
Christian Popescu <christian.popescu@outlook.com>
2024-10-28 23:54:14 +0100

    poc 25 - add label to window

--- 25-PocJavaSwing/src/PocSwingApp/src/main/java/fr/vadc/App.java
+++ 25-PocJavaSwing/src/PocSwingApp/src/main/java/fr/vadc/App.java
@@ -1,6 +1,7 @@
 package fr.vadc;
 
 import java.awt.*;
+import java.awt.event.*;
 import javax.swing.*;
 
 /**
@@ -23,8 +24,18 @@ class SimpleFrame extends JFrame
 private static final int DEFAULT_WIDTH = 300;
 private static final int DEFAULT_HEIGHT = 200;
 
+private JLabel label = new JLabel();
+private JPanel panel = new JPanel();
 public SimpleFrame()
 {
-setSize(DEFAULT_WIDTH, DEFAULT_HEIGHT);
+	setSize(DEFAULT_WIDTH, DEFAULT_HEIGHT);
+	addWindowListener(new WindowAdapter( ) {
+public void windowClosing(WindowEvent e) {
+System.exit(0);
+}
+});
+	label.setText("Hello World!");
+	panel.add(label);
+	add(panel);
 }
 }
