edd0e4e94f77a6f3fdeee6449d469f85d19c63fd
Christian Popescu <christian.popescu@outlook.com>
2024-11-11 23:59:26 +0100

    Add keybord present

--- 27-PocWinRTConsole/src/WinRTConsoleApp/WinRTConsoleApp/main.cpp
+++ 27-PocWinRTConsole/src/WinRTConsoleApp/WinRTConsoleApp/main.cpp
@@ -2,10 +2,14 @@
 
 using namespace winrt;
 using namespace Windows::Foundation;
+using namespace Windows::Devices::Input;
 
 int main()
 {
     init_apartment();
     Uri uri(L"http://aka.ms/cppwinrt");
     printf("Hello, %ls!\n", uri.AbsoluteUri().c_str());
+    KeyboardCapabilities kc = KeyboardCapabilities();
+    printf("Keybord present: %s \n", kc.KeyboardPresent() != 0 ? "Yes": "No");
+    
 }
