8ff1fa6cd32ac689d1c3675a844a93b263958587
Christian Popescu <christian.popescu@outlook.com>
2024-11-21 01:01:51 +0100

    29-PocCmakeCpp added

---
+++ 29_PocCmakeCpp/src/PocCmakeCpp/PocCmakeCpp/PocCmakeCpp.cpp
@@ -0,0 +1,12 @@
+﻿// PocCmakeCpp.cpp : Defines the entry point for the application.
+//
+
+#include "PocCmakeCpp.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
