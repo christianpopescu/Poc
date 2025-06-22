219b855006b2955b9dc1439a65975dbe3fbc4e9d
Christian Popescu <christian.popescu@outlook.com>
2025-06-22 18:48:35 +0200

    Add a second method to list notebook + WindowsForm on the fly dialog that selects the procedures to run

--- 32-PocCSharpOneNote/src/PocCSharOneNote/Program.cs
+++ 32-PocCSharpOneNote/src/PocCSharOneNote/Program.cs
@@ -1,17 +1,23 @@
 ﻿using System;
+using System.Linq;
+using System.Windows.Forms;
 using System.Xml.Linq;
 using Microsoft.Office.Interop.OneNote;
+
 namespace PocCSharOneNote
 {
     internal class Program
     {
+        [STAThread]
         static void Main(string[] args)
         {
-            // ListAllPages();
-            ListAllNoteBooks();
+            System.Windows.Forms.Application.EnableVisualStyles();
+            System.Windows.Forms.Application.SetCompatibleTextRenderingDefault(false);
+            System.Windows.Forms.Application.Run(new ProcedureSelectorForm());
         }
 
-        private static void ListAllPages()
+        // Procedures to select from
+        public static void ListAllPages()
         {
             var app = new Microsoft.Office.Interop.OneNote.Application();
             app.GetHierarchy(null, HierarchyScope.hsPages, out string xml);
@@ -24,11 +30,11 @@ namespace PocCSharOneNote
 
             foreach (var title in pageTitles)
             {
-                Console.WriteLine(title);
+                Console.WriteLine($"Page Title {title}");
             }
         }
 
-        private static void ListAllNoteBooks()
+        public static void ListAllNoteBooks()
         {
             var app = new Microsoft.Office.Interop.OneNote.Application();
             app.GetHierarchy(null, HierarchyScope.hsNotebooks, out string xml);
@@ -39,8 +45,86 @@ namespace PocCSharOneNote
                                     .ToList();
             foreach (var title in notebookTitles)
             {
-                Console.WriteLine(title);
+                Console.WriteLine($"Notebook Title: {title}");
             }
+        }
+
+        public static void ListAllNotebooks_v2()
+        {
+            var app = new Microsoft.Office.Interop.OneNote.Application();
+            app.GetHierarchy(null, HierarchyScope.hsNotebooks, out string xml);
+            var doc = XDocument.Parse(xml);
+            var ns = doc.Root?.Name.Namespace;
+            var notebooks = doc.Descendants(ns + "Notebook")
+                               .Select(nb => new
+                               {
+                                   Name = nb.Attribute("name")?.Value,
+                                   ID = nb.Attribute("ID")?.Value
+                               })
+                               .ToList();
+            foreach (var notebook in notebooks)
+            {
+                Console.WriteLine($" Notebook Info -> Notebook: {notebook.Name}, ID: {notebook.ID}");
+            }
+        }
+    }
+
+    public class ProcedureSelectorForm : Form
+    {
+        private ListBox listBox;
+        private Button runButton;
+
+        public ProcedureSelectorForm()
+        {
+            Text = "Select Procedure";
+            Width = 300;
+            Height = 200;
+
+            listBox = new ListBox
+            {
+                Dock = DockStyle.Top,
+                Height = 100
+            };
+            listBox.Items.AddRange(new string[]
+            {
+                "ListAllPages",
+                "ListAllNoteBooks",
+                "ListAllNotebooks_v2"
+            });
+
+            runButton = new Button
+            {
+                Text = "Run",
+                Dock = DockStyle.Bottom
+            };
+            runButton.Click += RunButton_Click;
+
+            Controls.Add(listBox);
+            Controls.Add(runButton);
+        }
+
+        private void RunButton_Click(object sender, EventArgs e)
+        {
+            var selected = listBox.SelectedItem as string;
+            if (selected == null)
+            {
+                MessageBox.Show("Please select a procedure.");
+                return;
+            }
+
+            switch (selected)
+            {
+                case "ListAllPages":
+                    Program.ListAllPages();
+                    break;
+                case "ListAllNoteBooks":
+                    Program.ListAllNoteBooks();
+                    break;
+                case "ListAllNotebooks_v2":
+                    Program.ListAllNotebooks_v2();
+                    break;
+            }
+
         }
     }
 }
